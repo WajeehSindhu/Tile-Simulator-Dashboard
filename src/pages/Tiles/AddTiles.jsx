2d034b0f2d126008a652933044146c4ceb607b10
Wajeeh Sindhu <wajeehulhassan91@gmail.com>
2025-06-19 12:36:51 +0500

    Same Ties

--- src/pages/Tiles/AddTiles.jsx
+++ src/pages/Tiles/AddTiles.jsx
@@ -241,7 +241,7 @@ const AddTiles = () => {
     return () => {
       isMounted = false;
     };
-  }, [id, isEditing]);
+  }, [id, isEditing, tiles,fetchTiles,fetchTileColors,fetchTileCategories,fetchGroutShapes,fetchShapeStyles,fetchScaleRange]);
 
   // Handle form submission
   const handleSubmit = async (e) => {
@@ -892,10 +892,10 @@ const AddTiles = () => {
                             : "border-gray-200"
                         }`}
                         style={{
-                          backgroundColor: 'transparent',
+                          backgroundColor: 'noBackground',
                           backgroundImage: 'url("data:image/svg+xml,%3Csvg width=\'20\' height=\'20\' viewBox=\'0 0 20 20\' xmlns=\'http://www.w3.org/2000/svg\'%3E%3Cg fill=\'%239C92AC\' fill-opacity=\'0.2\' fill-rule=\'evenodd\'%3E%3Ccircle cx=\'3\' cy=\'3\' r=\'3\'/%3E%3Ccircle cx=\'13\' cy=\'13\' r=\'3\'/%3E%3C/g%3E%3C/svg%3E")'
                         }}
-                        title="No Background"
+                        title="noBackground"
                       />
                       {tileColors.map((color) => (
                         <button
