9e0bff086ba827f118af25be77f1396f99fba638
Wajeeh Sindhu <wajeehulhassan91@gmail.com>
2025-06-28 12:50:39 +0500

    Adding Frontend url in it

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,12 @@ app.use(express.urlencoded({ extended: true }));
 // CORS configuration
 app.use(
   cors({
-    origin: "https://tile-simulator-dashboard.vercel.app",
+     origin: [
+      "https://tile-simulator-dashboard.vercel.app",
+      "https://lili-tiles.vercel.app",
+      "http://localhost:5173",
+      "http://localhost:5174"
+    ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization", "Accept"],
